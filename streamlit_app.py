b8bfb557d217cb8624003d63d3f2c19b2bac8704
PWC <woodycourt@gmail.com>
2021-10-08 16:18:05 +0100

    Random images working

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,16 @@
 import streamlit as st
+from PIL import Image
+import random
+import os
 
-header = st.beta_container()
 
-with header:
-    st.title('This OCT does not exist')
+st.title('This OCT does not exist')
+
+path = r"/Users/peterwoodward-court/Documents/thisoctdoesnotexist/images/"
+random_image = random.choice([
+    x for x in os.listdir(path)
+    if os.path.isfile(os.path.join(path, x))
+])
+image = Image.open('/Users/peterwoodward-court/Documents/thisoctdoesnotexist/images/'+ random_image)
+st.image(image)
+
